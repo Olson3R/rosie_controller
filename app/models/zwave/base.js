f8aef221e6534bb9bf20262f0c9e7525b9c8059f
Scott Olson <scott.olson@sportngin.com>
2015-04-13 17:24:57 -0500

    Remove cyclical dependency and cleanup

--- app/models/zwave/base.js
+++ app/models/zwave/base.js
@@ -1,44 +1,27 @@
 var zwave = require('../../../lib/zwave')
-var MultilevelSceneSwitch = require('./multilevel_scene_switch')
-var BinarySceneSwitch = require('./binary_scene_switch')
-//var _ = require('underscore')
 
-//var Base = function() {
 function Base(id, node) {
   this.id = id
   this.node = node
- //this.extend = function(child) {
-    //return _.extend({}, this, child)
-  //}
 }
 
-Base.prototype.test = function() {
-  console.log("woo")
+Base.find = function(id) {
+  id = parseInt(id, 10)
+  node = zwave.nodes[id]
+  if (node) {
+    return getNodeInstance(id, node)
+  }
 }
-
-//Base.extend = function(child) {
-  //return _.extend({}, this, child)
-//}
-
-//var Base = {
-  //extend: function(child) {
-    //return _.extend({}, this, child)
-  //},
-  //find: function(id) {
-    //id = parseInt(id, 10)
-    //node = zwave.nodes[id]
-    //if (node) {
-      //return getNodeInstance(id, node)
-    //}
-  //}
-//}
 
 var getNodeInstance = function(id, node) {
+  var MultilevelSceneSwitch = require('./multilevel_scene_switch')
+  var BinarySceneSwitch = require('./binary_scene_switch')
+
   switch(node.type) {
     case "Binary Scene Switch":
       return new BinarySceneSwitch(id, node)
     case "Multilevel Scene Switch":
-      return new MultilevelSceneSwitch(id, node) 
+      return new MultilevelSceneSwitch(id, node)
     default:
       return new Base(id, node)
   }
