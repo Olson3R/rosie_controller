ed82438928c8467166c686a60c6c61c6e44ffb9c
scott <scott@mini-pc>
2015-04-19 10:25:09 -0500

    Use service classes for each command class.

--- app/models/zwave/base.js
+++ app/models/zwave/base.js
@@ -1,4 +1,7 @@
 var zwave = require('../../../lib/zwave')
+var SwitchBinary = require('./classes/switch_binary')
+var SwitchMultilevel = require('./classes/switch_multilevel')
+var _ = require('underscore')
 
 function Base(id, node) {
   this.id = id
@@ -9,22 +12,22 @@ Base.find = function(id) {
   id = parseInt(id, 10)
   node = zwave.nodes[id]
   if (node) {
-    return getNodeInstance(id, node)
+    return new Base(id, node)
   }
 }
 
-var getNodeInstance = function(id, node) {
-  var MultilevelSceneSwitch = require('./multilevel_scene_switch')
-  var BinarySceneSwitch = require('./binary_scene_switch')
-
-  switch(node.type) {
-    case "Binary Scene Switch":
-      return new BinarySceneSwitch(id, node)
-    case "Multilevel Scene Switch":
-      return new MultilevelSceneSwitch(id, node)
-    default:
-      return new Base(id, node)
-  }
+Base.prototype.update = function(payload) {
+  var self = this
+  _.each(payload, function(value, key) {
+    switch(key) {
+      case '37': // COMMAND_CLASS_SWITCH_BINARY
+        new SwitchBinary(self.id, self.node).update(value)
+        break
+      case '38': // COMMAND_CLASS_SWITCH_MULTILEVEL
+        new SwitchMultilevel(self.id, self.node).update(value)
+        break
+    }
+  })
 }
 
 module.exports = Base
