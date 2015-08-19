099085a5937306ebf75dfba696183916003645da
olson3r <scootles.olson@gmail.com>
2015-08-19 00:16:47 -0500

    Moved to rosie_controller_remotes plugin

--- plugins/rosie_controller_remotes/lib/remotes.js
+++
@@ -1,9 +0,0 @@
-var RemoteModel = require('./remote_model')
-var routes = require('./routes')
-
-var Remotes = function(server, options) {
-  RemoteModel.configure(options)
-  routes(server)
-}
-
-module.exports = Remotes
